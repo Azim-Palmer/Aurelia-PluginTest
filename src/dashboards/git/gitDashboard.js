609d69eb746c54e7d6c653de1c7eff5ed70039f9
Az <azim.palmer@googlemail.com>
2015-07-07 12:58:18 +0100

    updating to use transform updates

--- src/dashboards/git/gitDashboard.js
+++ src/dashboards/git/gitDashboard.js
@@ -10,7 +10,6 @@ export class GitDashboard{
   constructor () {
 
     var componentsForCompose = [
-      //{id: 1, viewModel:'dash-component/components/baseComponent/baseComponent'}
       {id: 1, viewModel:'dash-github/components/singleValueComponent/gitSingleValueComponent'},
       {id: 1, viewModel:'dash-github/components/imageGrid/imageGrid'}
     ];
@@ -21,32 +20,9 @@ export class GitDashboard{
   }
 
   activate(params) {
-    //this.params = params;
-    //this.clientId = params.clientId;
-    var that = this;
   }
 
   attached() {
-  }
-
-
-
-  buildScreen(screenInfo) {
-    for(let activeComponent of screenInfo.ActiveComponents)
-    {
-      var componentPath = activeComponent.Component.ComponentPath;
-
-      var propertyValueObj = {};
-
-      for(let pv of activeComponent.PropertyValues)
-      {
-        propertyValueObj[pv.Property.Name] = pv.Value;
-      }
-
-      this.components.push({component: activeComponent.Component, propertyValues : propertyValueObj,type:'components/'+componentPath+'/'+componentPath+'-component'});
-
-    }
-
   }
 }
 
